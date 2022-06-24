5f3c716fb80e2813ab1ec1ee78430b1e608039d9
Reyyen cuneyt <rrayyan213@gmail.com>
2022-06-24 13:46:03 +0300

    All Project(success)

---
+++ src/main/java/com/example/java_primary/JavaPrimaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_primary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPrimaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPrimaryApplication.class, args);
+	}
+
+}
